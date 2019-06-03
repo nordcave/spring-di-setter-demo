80c77c2db2dbd3a453c12c9834c30ff400d3fdec
nordcave <nadraga.oleg@gmail.com>
2019-06-03 11:23:28 +0300

    Revert "prototype and singleton"
    This reverts commit 569cda05b9f1cc7a3bcbc0f1248e9dd78639d4b5.

--- src/main/java/nord/springdemo/DISetterApp.java
+++ src/main/java/nord/springdemo/DISetterApp.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import nord.springdemo.domain.Organization;
 
-public class BeanScopeApp {
+public class DISetterApp {
 
 	public static void main(String[] args) {
 		// create the application context (container)
@@ -13,22 +13,14 @@ public class BeanScopeApp {
 		
 		// create the bean
 		Organization org = (Organization) ctx.getBean("myorg");
-		Organization org2 = (Organization) ctx.getBean("myorg");
-		org2.setPostalCode("98989");
 		
 		// invoke the company slogan via the bean
-		//System.out.println(org.corporateSlogan());
+		System.out.println(org.corporateSlogan());
 		
 		// Print Organization details
 		System.out.println(org);
-		System.out.println(org2);
-		if(org == org2) {
-			System.out.println("Singleton scope test: org and org2 point to the same instance");
-		} else {
-			System.out.println("Both org and org2 are separate instances");
-		}
 		
-		//System.out.println(org.corporateService());
+		System.out.println(org.corporateService());
 		
 		// close the application context (container)
 		((ClassPathXmlApplicationContext) ctx).close();
